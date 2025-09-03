ff56c331f877c650ecaf1fb60507cd3b09e169ac
Kedar Dhage <kedardhage0@gmail.com>
2025-09-03 21:47:24 +0530

    Arithmetic operation in Java

--- FJD_NOTES/src/Class_8/Arithmetic.java
+++ FJD_NOTES/src/Class_8/Arithmetic.java
@@ -1,4 +1,5 @@
 package Class_8;
+import java.util.Scanner;
 
 public class Arithmetic {
     public static void main(String[] args) {
@@ -11,6 +12,26 @@ public class Arithmetic {
         System.out.println("a = "+ a +" b = "+b +" mul = "+ (a * b));
         System.out.println("a = "+ a +" b = "+b +" div = "+ (a / b));
         System.out.println("a = "+ a +" b = "+b +" rem = "+ (a % b));
+
+            // Here the same way but takeing the input from the user
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Enter a first number: ");
+//        int a = in.nextInt();
+//        System.out.print("Enter a second  number: ");
+//        int b = in.nextInt();
+//
+//        int add = a + b;
+//        int sub = a - b;
+//        int mul = a * b;
+//        int div = a / b;
+//        int rem = a % b;
+//        System.out.println("The Addition of two number is:" + add);
+//        System.out.println("The substraction of two number is:" + sub);
+//        System.out.println("The Multipication of two number is:"+ mul);
+//        System.out.println("The Division of two number is:"+div);
+//        System.out.println("The reminder of the two number is"+rem);
+
 
     }
 }

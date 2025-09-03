696786c5c3330734920600880b7125fe184bf18c
Kedar Dhage <kedardhage0@gmail.com>
2025-09-03 21:36:42 +0530

    Arithmetic Operation in Java

---
+++ FJD_NOTES/src/Class_8/Arithmetic.java
@@ -0,0 +1,16 @@
+package Class_8;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        System.out.println("Arithmetic Operation in Java");
+        int  a = 10 ;
+        int b = 20;
+
+        System.out.println("a = "+ a +" b = "+b +" sum = "+ (a + b));
+        System.out.println("a = "+ a +" b = "+b +" sub = "+ (a - b));
+        System.out.println("a = "+ a +" b = "+b +" mul = "+ (a * b));
+        System.out.println("a = "+ a +" b = "+b +" div = "+ (a / b));
+        System.out.println("a = "+ a +" b = "+b +" rem = "+ (a % b));
+
+    }
+}

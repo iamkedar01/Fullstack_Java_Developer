17ab555b48f65f3b9ea20380cf9c3c60fa0d1421
Kedar Dhage <kedardhage0@gmail.com>
2025-09-04 21:15:30 +0530

    Relational Operation in Java

---
+++ FJD_NOTES/src/Class_8/RelationOperation.java
@@ -0,0 +1,23 @@
+package Class_8;
+
+public class RelationOperation {
+    public static void main(String[] args) {
+        System.out.println(" \nRelation operation in java");
+        /*  Relational Operators => >, <, >=, <=, ==, != (not equal)
+                                    Grater ,Less, graterthen , lessthen , equals , notequal
+	- used to form the conditions (comparisions)  */
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("Eligible to drive");
+        }
+        else {
+            System.out.println("your not eligible to vote ");
+            }
+
+        String name = "Kedar";
+        if(name == "Kedar" ){
+            System.out.println("Welcome to the java");
+        }
+        }
+    }
+

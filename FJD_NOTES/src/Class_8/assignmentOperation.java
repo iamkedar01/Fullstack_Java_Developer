d511dbca065dfefd0f20615ffeb5d03aaca3fa4c
Kedar Dhage <kedardhage0@gmail.com>
2025-09-06 22:52:06 +0530

    Assignment Operation In Java

---
+++ FJD_NOTES/src/Class_8/assignmentOperation.java
@@ -0,0 +1,17 @@
+package Class_8;
+
+public class assignmentOperation {
+    public static void main(String[] args) {
+        System.out.println("Assignment operation in Java");
+        int x = 5;
+       // x = x + 5;//x = 5 + 5 = 10
+        x += 5; // Here we assign the 5 value
+        // use the operator like +, -, * , / = equal after that give the value
+        System.out.println(x);
+
+        int y = 10;
+        //y = y * 20;//y = 10 * 20 = 200
+        y *= 20; // use the operator like +, -, * , / = equal after that give the value
+        System.out.println(y);
+    }
+}
